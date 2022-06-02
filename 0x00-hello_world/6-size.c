0d63b086310eb605c371c5ab0282772807913f68
Your Name <you@example.com>
2022-06-02 16:12:33 -0700

    New

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
  *main - main block
  *return 0
  */
-int main()
+int main(void)
 {
         printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
