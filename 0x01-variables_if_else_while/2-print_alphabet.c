30cee12703a6825a9bb2ce71fc752732805b74a8
Your Name <you@example.com>
2022-06-03 14:19:59 -0700

    More loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
