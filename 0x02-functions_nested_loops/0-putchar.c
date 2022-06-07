00ebf6e51dd3bf3c9d69c46c9596c6330691d652
Your Name <you@example.com>
2022-06-07 10:49:40 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *sh = "putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * print_alphabet - a finction that prints alphabet, in lowercase
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n<= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
