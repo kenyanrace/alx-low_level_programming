0a1dc4de291847a90bfa08cabb23d4aa1390a92b
Your Name <you@example.com>
2022-06-07 19:39:20 -0700

    new

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,16 @@
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
-{	
-	if ((c >= 'A' && c >= 'Z') || (c >= 'a' && c <= 'z'))
-		return (1);
+{
+	int i;
+
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		i = 1
+	}
 	else
-		return (0);
-	
+	{
+		i = 0
+	}
+	return (i);
 }
