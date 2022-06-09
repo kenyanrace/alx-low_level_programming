e58d451a77980c7ccb62ced98d4cf7217bd1a9c5
Your Name <you@example.com>
2022-06-09 16:19:12 -0700

    adv

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _isdigit - checks if character is a digit
+ * @c: character to be checked
+ * Return: 1 if c is a digit else 0
+ */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+nclude "main.h"
+/**
+ * mul - multiplies numbers
+ * @a: first number
+ * @b: second integer
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
