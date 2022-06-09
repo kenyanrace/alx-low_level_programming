483b917de63542cc0bce1a4c0507fb5b359818c5
Your Name <you@example.com>
2022-06-09 15:43:45 -0700

    adv

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: Always 0
+ */
+int main(void)
+{
+	long i, n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i ==0)
+		{
+			n = n / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(void)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
