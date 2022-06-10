7a1c75ba2db28feaacac0865591867981a56cbe4
Your Name <you@example.com>
2022-06-10 12:47:31 -0700

    Adv

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_ diagonal - prints a character doagionallly
+ * print_ diagonal - prints a character diagonal
  * @n: variable
  * Return: Always 0
  */
@@ -8,14 +8,13 @@ void print_diagonal(int n)
 {
 	int x, y;
 
-
 	if (n > 0)
 	{
 		for (x = 1; x <= n; x++)
 		{
 			for (y = 1; y <= n; y++)
 			{
-				if (x ==y)
+				if (x == y)
 				{
 					_putchar(92);
 					break;
