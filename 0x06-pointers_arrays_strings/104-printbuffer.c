349a5c30132af1bf30477fca18ef7cd324ff1adf
Your Name <you@example.com>
2022-06-17 11:17:47 -0700

    old

---
+++ 0x06-pointers_arrays_strings/104-printbuffer.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_Line - prints  bytes for  a buffer
+ * @c: buffer to print
+ * @s: bytes
+ * @l: lines
+ * Return: void
+ */
+void printline(char *c, int s, int l)
+{
+	int h, k;
+
+	for (j = o; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		prinf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
+}
