bd295a9117958cfc5791fa911bdf872f7a8e5186
Your Name <you@example.com>
2022-06-24 18:21:19 -0700

    nod

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h" 
+#include "main.h"
 /**
  * main - buffer
  * @argc: arguments
@@ -9,7 +9,6 @@
 int main(int argc, char *argv[])
 {
 	(void)argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
