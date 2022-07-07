28675bd9aa558d6a11219e3129b4ae8ef8ed278f
Your Name <you@example.com>
2022-07-07 12:07:49 -0700

    easy

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,6 +12,8 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i;
 
+	if (n == 0)
+		return (0);
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
@@ -19,4 +21,5 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	va_end(valist);
 	return (sum);
+
 }
