49e35258705006800b4ea2f4a83ee0bedb95fd15
Your Name <you@example.com>
2022-07-07 00:03:51 -0700

    variadic_functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - jdhytrfghuytgfv
+ * @n: numhgf
+ * Return: jhhjdjjkdklkmsd
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	int sum = 0;
+	unsigned int i;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, int);
+	}
+	va_end(valist);
+	return (sum);
+}
